79e74d79a200c5ee24eef8d3cb7d92a8279cb2f4
Ty Paulhus <ty@sintaxera.com>
2015-12-23 14:28:40 -0500

    LDTD note templates

--- ldtd-environmental.php
+++ ldtd-environmental.php
@@ -3,77 +3,81 @@
 Template Name: LDTD - Environmental
 */
 
-get_header(); ?> 
-<h1>Laser Diode Thermal Desorption - Application Notes</h1>
-<h2 class="pageSubTitle"><?php the_title(); ?></h2>
+get_header(); ?>
 
-<?php 
-	if (have_posts()) :
+<?php get_template_part( 'sidebar' , 'about'); ?>
 
-		while (have_posts()) : the_post();
-			
-			the_content();
+<div class="col-sm-9">
+	<h1>Laser Diode Thermal Desorption - Application Notes</h1>
+	<h2 class="pageSubTitle"><?php the_title(); ?></h2>
 
-		endwhile;
+	<?php
+		if (have_posts()) :
 
-		else : 
-			//fallback content
-	endif;
+			while (have_posts()) : the_post();
 
-	// LDTD notes begin
-	$ldtd = new WP_Query('post_type=ldtd_note&cat=3');
+				the_content();
 
-	if ($ldtd->have_posts()) :
+			endwhile;
 
-		while ($ldtd->have_posts()) : $ldtd->the_post(); ?>
+			else :
+				//fallback content
+		endif;
 
-		<div class="note">
-			<header>
-				<h3 class="note-title"><?php the_title(); ?></h3>
-			</header>
-			<ul class="note-info">
-				<li><strong>Partnership: </strong><?php the_field('partnership'); ?></li>
-				<a href="<?php the_field('file'); ?>">Download</a>
-				<li><strong>Molecules: </strong><?php the_field('molecules'); ?></li>
-				<li><strong>Matrix: </strong><?php the_field('matrix'); ?></li>
-				<li><strong>Extraction Method: </strong><?php the_field('extraction_method'); ?></li>
-				<li><strong>Instrumentation: </strong><?php the_field('instrumentation'); ?></li>
-				<li><strong>No. Code: </strong><?php the_field('code'); ?></li>
-				<li><strong>Category: </strong><?php the_field('category'); ?></li>
-			</ul>
-		</div>
+		// LDTD notes begin
+		$ldtd = new WP_Query('post_type=note&cat=9');
 
-		<?php endwhile;
+		if ($ldtd->have_posts()) :
 
-		else : 
-			//fallback content
-	endif;
-	wp_reset_postdata();
+			while ($ldtd->have_posts()) : $ldtd->the_post(); ?>
 
-	// LDTD articles begin
-	$ldtd = new WP_Query('post_type=ldtd_article&cat=3');
-
-	if ($ldtd->have_posts()) :
-
-		while ($ldtd->have_posts()) : $ldtd->the_post(); ?>
-
-			<div class="ldtd-article">
-			<header>
-				<h1 class="article-title"><?php the_title(); ?></h1>
-			</header>
-			<div class="article-info">
-				<a href="<?php the_field('file'); ?>">Download</a>
+			<div class="note">
+				<header>
+					<h3 class="note-title"><?php the_title(); ?></h3>
+				</header>
+				<ul class="note-info">
+					<li><strong>Partnership: </strong><?php the_field('partnership'); ?></li>
+					<a href="<?php the_field('file'); ?>">Download</a>
+					<li><strong>Molecules: </strong><?php the_field('molecules'); ?></li>
+					<li><strong>Matrix: </strong><?php the_field('matrix'); ?></li>
+					<li><strong>Extraction Method: </strong><?php the_field('extraction_method'); ?></li>
+					<li><strong>Instrumentation: </strong><?php the_field('instrumentation'); ?></li>
+					<li><strong>No. Code: </strong><?php the_field('code'); ?></li>
+					<li><strong>Category: </strong><?php the_field('category'); ?></li>
+				</ul>
 			</div>
-		</div>
 
-		<?php endwhile;
+			<?php endwhile;
 
-		else : 
-			//fallback content
-	endif;
-	wp_reset_postdata();
+			else :
+				//fallback content
+		endif;
+		wp_reset_postdata();
 
-	?>
+		// LDTD articles begin
+		$ldtd = new WP_Query('post_type=article&cat=9');
+
+		if ($ldtd->have_posts()) :
+
+			while ($ldtd->have_posts()) : $ldtd->the_post(); ?>
+
+				<div class="ldtd-article">
+				<header>
+					<h1 class="article-title"><?php the_title(); ?></h1>
+				</header>
+				<div class="article-info">
+					<a href="<?php the_field('file'); ?>">Download</a>
+				</div>
+			</div>
+
+			<?php endwhile;
+
+			else :
+				//fallback content
+		endif;
+		wp_reset_postdata();
+
+		?>
+</div><!-- end col-9 -->
 
 <?php get_footer(); ?>
-
--- sidebar-about.php
+++ sidebar-about.php
@@ -5,13 +5,13 @@
  * @package overbrook
  */
 ?>
-	<div class="sidebar leftNav col-sm-12 col-md-3" role="complementary">
+	<div class="leftNav col-sm-12 col-md-3" role="complementary">
 
 			<?php wp_nav_menu( array('menu' => 'sidenav', 'menu_class' => 'sideNav' )); ?>
 
 			<div class="atAGlance">
 				<h4>At-a-Glance</h4>
 				<a href="<?php bloginfo('template_directory'); ?>/docs/Overbrook_At-a-Glance_Final.pdf">Download</a>
-			</div>			
+			</div>
 
 	</div><!-- Sidebar -->
